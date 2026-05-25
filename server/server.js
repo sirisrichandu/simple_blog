d6a5d4c91c88cfcf0f839ce801a685fa879b5954
unknown <sirisrichandu@gmail.com>
2026-05-25 11:02:36 +0530

    fixed deploying error

--- server/server.js
+++ server/server.js
@@ -5,46 +5,22 @@ require("dotenv").config();
 
 const app = express();
 
-// CORS
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://simple-blog-omega-puce.vercel.app",
-      "https://simple-blog-rd2o7o30f-sirisrichandus-projects.vercel.app"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true
-  })
-);
-
-// Handle preflight requests
-app.options(/.*/, cors());
+app.use(cors());
 
 app.use(express.json());
 
-// Routes
 app.use("/api/blogs", require("./routes/blogRoutes"));
 
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB Connected");
-  })
-  .catch((err) => {
-    console.log("MongoDB Error:", err);
-  });
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("MongoDB Connected"))
+.catch(err=> console.log(err));
 
-// Test route
-app.get("/", (req, res) => {
-  res.send("API Running");
+app.get("/",(req,res)=>{
+ res.send("API Running");
 });
 
-// Server start
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(PORT,()=>{
+ console.log(`Server running on port ${PORT}`);
 });
