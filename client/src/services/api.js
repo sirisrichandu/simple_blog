cb56bb8fb4a2983b56302116bc8bf11768026a23
unknown <sirisrichandu@gmail.com>
2026-05-25 12:08:31 +0530

    fixed backend url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://simple-blog-9940.onrender.com/api/blogs"
+  baseURL: "https://simple-blog-994o.onrender.com/api/blogs"
 });
 
 export const getBlogs = () => API.get("/");
