fb0bd100e5c617d579110793a263761be78374e4
unknown <sirisrichandu@gmail.com>
2026-05-25 11:12:05 +0530

    final cors test

--- server/server.js
+++ server/server.js
@@ -5,22 +5,31 @@ require("dotenv").config();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 
 app.use(express.json());
 
+// Routes
 app.use("/api/blogs", require("./routes/blogRoutes"));
 
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log("MongoDB Connected"))
-.catch(err=> console.log(err));
-
-app.get("/",(req,res)=>{
- res.send("API Running");
+.then(() => {
+  console.log("MongoDB Connected");
+})
+.catch((err) => {
+  console.log("MongoDB Error:", err);
 });
 
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+// Server Start
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
- console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
