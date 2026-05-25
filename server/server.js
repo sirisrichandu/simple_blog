62114bdbd302cada7197a647d83b6122d9038774
unknown <sirisrichandu@gmail.com>
2026-05-25 10:27:02 +0530

    fixed cors

--- server/server.js
+++ server/server.js
@@ -5,21 +5,14 @@ require("dotenv").config();
 
 const app = express();
 
-// Middleware
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://simple-blog-omega-puce.vercel.app"
-    ],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true
-  })
-);
+app.use(cors({
+  origin: "https://simple-blog-omega-puce.vercel.app",
+  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
+  credentials: true
+}));
 
 app.use(express.json());
 
-// Routes
 app.use("/api/blogs", require("./routes/blogRoutes"));
 
 mongoose.connect(process.env.MONGO_URI)
@@ -34,7 +27,7 @@ app.get("/",(req,res)=>{
  res.send("API Running");
 });
 
-const PORT=process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
  console.log(`Server running on port ${PORT}`);
