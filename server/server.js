14abedee98ceb8426d04091e697348d039cab943
unknown <sirisrichandu@gmail.com>
2026-05-25 07:48:22 +0530

    Fixed MongoDB connection and deployment setup

--- server/server.js
+++ server/server.js
@@ -5,22 +5,30 @@ require("dotenv").config();
 
 const app = express();
 
-// middleware
-
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/blogs",require("./routes/blogRoutes"));
+
+// Routes
+app.use("/api/blogs", require("./routes/blogRoutes"));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB Connected");
+})
+.catch((err) => {
+    console.log("MongoDB Error:", err);
+});
 
-// test route
-app.get("/", (req,res)=>{
+// Test route
+app.get("/", (req, res) => {
     res.send("API Running");
 });
 
-app.listen(5000,()=>{
-    console.log("Server running on port 5000");
+// Server start
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
